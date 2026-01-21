495baa9ec6f486c05e8cdcbc881838480915ce65
Martin Kurtz <70766440+rakurtz@users.noreply.github.com>
2026-01-21 17:22:35 +0700

    improved exports of models

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { MaraDocsClient } from "./MaraDocsClient/index";
 export { MaraDocsServer } from "./MaraDocsServer/index";
 
+// Error types for catch blocks
 export { ApiErrorType, ApiErrorException } from "./models/errors";
 export type { HttpErrorResponse, ApiError } from "./models/errors";
