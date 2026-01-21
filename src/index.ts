de503ec4549bb54a8e776f8dfc70b9479b3806be
Martin Kurtz <70766440+rakurtz@users.noreply.github.com>
2026-01-21 17:37:17 +0700

    changing the way we export things (relying on subpathes in package.json)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
-export { API_URL_V1 } from "./shared/apiUrl";
 export { MaraDocsClient } from "./MaraDocsClient/index";
 export { MaraDocsServer } from "./MaraDocsServer/index";
 
-// Error types for catch blocks
-export { ApiErrorType, ApiErrorException } from "./models/errors";
-export type { HttpErrorResponse, ApiError } from "./models/errors";
+// /models and /shared are exported as subpathes in package.json
